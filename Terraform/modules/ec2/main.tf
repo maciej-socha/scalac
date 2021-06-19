3949042f8631018e9f0426318c1baf0cad26818f
Socha <socham3@emea.roche.com>
2021-06-19 19:34:42 +0200

    Small changes in Ansible playbook

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -20,30 +20,4 @@ resource "aws_instance" "ec2_public" {
   tags = {
     "Name" = "${var.namespace}-EC2-PUBLIC"
   }
-
-  # Copies the ssh key file to home dir
-  provisioner "file" {
-    source      = "./${var.key_name}.pem"
-    destination = "/home/ec2-user/${var.key_name}.pem"
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("${var.key_name}.pem")
-      host        = self.public_ip
-    }
-  }
-  
-  //chmod key 400 on EC2 instance
-  provisioner "remote-exec" {
-    inline = ["chmod 400 ~/${var.key_name}.pem"]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("${var.key_name}.pem")
-      host        = self.public_ip
-    }
-
-  }
 }
